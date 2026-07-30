9955d2f94fb9f0fccd709ec8f1bd349b24ad9a76
Loranius <72103663+Loranius@users.noreply.github.com>
2026-07-30 14:20:11 +0300

    feat(tree-orientation): export leaf orientation API

---
+++ src/engine/leafOrientation/index.ts
@@ -0,0 +1,9 @@
+export { DEFAULT_TREE_LEAF_ORIENTATION_CONFIG } from './config';
+export { buildTreeLeafOrientation } from './treeLeafOrientation';
+export type {
+  BuildTreeLeafOrientationInput,
+  TreeLeafOrientationConfig,
+  TreeLeafOrientationLayerConfig,
+  TreeLeafOrientationProfile,
+  TreeLeafOrientationState,
+} from './types';
