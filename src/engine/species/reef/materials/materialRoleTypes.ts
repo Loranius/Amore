de2582674493d20a9e1fa173c966dfe5df0fa7c8
Loranius <72103663+Loranius@users.noreply.github.com>
2026-07-31 11:48:56 +0300

    feat(reef-materials): add material roles

---
+++ src/engine/species/reef/materials/materialRoleTypes.ts
@@ -0,0 +1,8 @@
+export type ReefMaterialRole =
+  | 'substrate-rock'
+  | 'branching-calcified'
+  | 'massive-calcified'
+  | 'plating-calcified'
+  | 'encrusting-calcified'
+  | 'soft-tissue'
+  | 'sea-fan-tissue';
